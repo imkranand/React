9703054ff857ff8b397c3178f99fd0a96505bf41
Kumar Anand <kranand.vnit@gmail.com>
2022-04-22 12:24:00 +0530

    Update state - Single state update approach (using one useState)

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,28 @@
+import React, {useState} from 'react';
 import ExpenseDate from '../Expenses/ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    // let title = props.title;
+    const [title,setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React');
+    const clickHandler = () => {
+        // title = 'Updated !!!'    //title can't be updated like this
+        // To update title we need to use the function setTitle
+        setTitle('Updated !!!');
+        console.log(title); // console.log is showing the old values of the variable
+    };
 
     return (
-        <div className = "expense-item">
+        <Card className = "expense-item">
             <ExpenseDate  date = {props.date}/>
             <div className = "expense-item__description">
-                <h2>{props.title}</h2>
-                <div className = "expense-item__price">{props.amount}</div>
+                <h2>{title}</h2>
+                <div className = "expense-item__price">$ {props.amount}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}> Change Title</button>
+        </Card>
     );
 }
 
